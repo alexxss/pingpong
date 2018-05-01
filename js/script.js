ab32fd85206954e474ecee3076c440200f8e6116
Glitch (ping-pong-bee) <none>
2018-05-01 16:16:30 +0000

    🛥🍅 Updated with Glitch

---
+++ js/script.js
@@ -0,0 +1,122 @@
+var canvas, context;
+var canon = new Image();
+var bg = new Image();
+var bricks = new Array(40);
+var balls = new Array(5);
+
+var cx=360;
+var cy=700;
+var dct=-1;    //主角的方向(0:左    1:右)
+
+var brickImg = function(){
+  this.image = new Image();
+  this.image.src = "https://cdn.glitch.com/7469c21b-4cd3-4c40-8cb3-0bf725adf90b%2Fif_honey-bee-insect-fly-beekeeping-apiary-apiculture_2189584.png";
+}
+
+var myBrick = function(x,y){
+  this.x=x;
+  this.y=y;
+  
+  this.draw=function(){
+    document.getElementById("canvas").getContext("2d").drawImage(this.image,this.x,this.y,70,70);
+  }
+  
+}
+
+var ballImg = function(){
+  this.image = new Image();
+  this.image.src="https://cdn.glitch.com/7469c21b-4cd3-4c40-8cb3-0bf725adf90b%2Fball.png";
+}
+
+var ball = function(x){
+  this.x = x;
+  this.y = cy;
+  this.spawn=false;
+  this.draw = function(){
+    if (this.spawn){
+      document.getElementById("canvas").getContext("2d").drawImage(this.image,this.x,this.y,20,20);
+    }
+  }
+}
+
+function init(){
+  canvas = document.getElementById("canvas");
+  context = canvas.getContext("2d");
+  
+  bg.addEventListener("load",setup,false);
+
+  canon.src="https://cdn.glitch.com/7469c21b-4cd3-4c40-8cb3-0bf725adf90b%2F0214.png";
+  bg.src="https://cdn.glitch.com/7469c21b-4cd3-4c40-8cb3-0bf725adf90b%2Fmap_2.png";
+  myBrick.prototype = new brickImg();
+  ball.prototype = new ballImg();
+    
+}
+
+function setup(){
+    
+
+  for(var i=0; i<5;i++){
+    balls[i] = new ball(cx+32);
+  } 
+  for(var i = 0; i < 40; i++) {
+    
+    var x = (i%10) * 80;
+    var y = parseInt(i/10) * 80;
+    bricks[i] = new myBrick(x,y);
+    
+  }
+
+  //****如果使用者按下鍵盤****	
+  document.onkeydown=function(e){
+
+    //如果按左
+    if(e.keyCode==37){dct=0;}       
+
+    //如果按右
+    if(e.keyCode==39){dct=1;} 
+
+    if(e.keyCode==32){
+      addBall();
+    }
+  }    
+  setInterval(showImg, 50);  
+}
+
+function addBall(){
+  for(var i=0;i<5;i++){
+    if(balls[i].spawn==false){
+      balls[i].x=cx+32;
+      balls[i].y=cy;
+      balls[i].spawn=true;
+      break;
+    }
+  }  
+}
+
+function showImg() {
+//   clear canvas
+  context.clearRect(0,0,canvas.width,canvas.height);
+  
+//   fill bg
+  context.drawImage(bg,0,0);
+  
+  // 畫圖
+  context.drawImage(canon,cx,cy);
+
+  // ****移動主角座標
+  if(dct==0 && cx>0){cx-=4;}
+  if(dct==1 && cx+4+80<800){cx+=4;}  
+  
+  for(var i=0; i<40;i++){
+    bricks[i].draw();
+  }
+  for(var i=0; i<5;i++){
+    if(balls[i].spawn){
+      if(balls[i].y+30>0){balls[i].y-=10;}
+      else {
+        balls[i].spawn=false;
+      }
+    }
+    balls[i].draw();
+  }
+}
